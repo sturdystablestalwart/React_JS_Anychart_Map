ea6d36fea485e564a2784a0be3583bdcc611590a
sturdystablestalwart <laosen9@gmail.com>
2023-08-11 12:58:52 +0300

    Minor cosmetic changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class ColumnChart extends Component {
   shouldComponentUpdate(nextProps) {
-    // Compare the relevant props to decide if re-rendering is needed
-    // For example, you might want to compare chart data or other properties
+    // compare the relevant props to decide if re-rendering is needed
+    // for example, you might want to compare chart data or other properties
     if (this.props.data !== nextProps.data) {
-      return true; // Re-render if data has changed
+      return true; // re-render if data has changed
     }
-    return false; // Avoid re-render if data hasn't changed
+    return false; // avoid re-render if data hasn't changed
   }
 
   componentDidMount() {
@@ -26,25 +26,26 @@ class ColumnChart extends Component {
 
   renderChart() {
     const data = [
-      { id: "AU.WA", value: 310 },
+      { id: "AU.NT", value: 100 },
+      { id: "AU.QL", value: 50 },
+      { id: "AU.NS", value: 25 },
+      { id: "AU.VI" },
+      { id: "AU.TS" },
       { id: "AU.JB" },
-      { id: "AU.NS", value: 240 },
-      { id: "AU.VI", value: 75 },
-      { id: "AU.NT", value: 130 },
-      { id: "AU.TS", value: 190 },
       { id: "AU.CT" },
       { id: "AU.SA" },
-      { id: "AU.QL" },
     ];
 
+    // set-up a map
     var map = anychart.map();
     map.geoData(anychart.maps.australia);
 
     // set the series
     var series = map.choropleth(data);
     series.stroke("#000 .3");
+
+    // set-up a color range for the series
     series.colorScale(anychart.scales.linearColor("#FFEBD6", "#C40A0A"));
-    series.labels().fontColor("black");
 
     // set the container
     map.container("chart-container");
